16db10e21931c1d1068c73f6d217dea6a59da444
Dimi <sztanko@gmail.com>
2021-02-16 21:43:38 +0000

    Add color scale for alignment accuracy

--- typescript/solsticestreets-site/components/CityMap.tsx
+++ typescript/solsticestreets-site/components/CityMap.tsx
@@ -1,13 +1,14 @@
 import { MapContainer, TileLayer, GeoJSON, useMap } from "react-leaflet";
 import { GeoJsonObject } from "geojson";
 import type { City } from "./City";
-
+import Link from "next/link";
+import * as d3 from "d3-scale";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
 import { Control, LatLngExpression } from "leaflet";
 
-function scale(domain: number, range: number) {
+function scaleOld(domain: number, range: number) {
   return (x: number) => {
     return (x * range) / domain;
   };
@@ -29,14 +30,14 @@ const StreetStatsControl = (props: {
   });
   const style = {
     width: props.width,
-    height: props.width * 1.2,
+    height: props.width + 100,
   };
 
   const histogram = props.stats.street_histogram.map(Math.round);
   console.log(histogram);
   const maxLength = Math.max(...histogram);
 
-  const histScale = scale(maxLength, r);
+  const histScale = d3.scaleLinear().domain([0, maxLength]).range([0, r]); // scale(maxLength, r);
   const step = Math.PI / 180;
   const xyFunc = (radius: number, azimuth: number) => {
     return [
@@ -44,8 +45,7 @@ const StreetStatsControl = (props: {
       radius * Math.sin(azimuth - Math.PI / 2),
     ];
   };
-  const csv = histogram.map((l: number, i: number) => `${Math.round(l)},${i}`);
-  console.log(csv.join("\n"));
+
   const histogramPath = histogram.map((l: number, i: number) => {
     // console.log(i);
     // console.log(l);
@@ -61,10 +61,27 @@ const StreetStatsControl = (props: {
     // const shape = `M0,0 L${p0[0]},${p0[1]} L${p1[0]},${p1[1]} L0.0`;
     return <path d={shape} key={i} id={`path_${i}`} />;
   });
+  console.log(props.stats);
+  const azimuthLines = props.stats.sun_azimuths.map((az: number, i: number) => {
+    const [x, y] = xyFunc(r, (az * Math.PI) / 180);
+    return (
+      <line
+        x1={-x}
+        y1={-y}
+        x2={x}
+        y2={y}
+        className={`azimuth_line sun_azimuth_${i}`}
+        key={i}
+      />
+    );
+  });
 
   return (
     <div className="leaflet-bottom leaflet-left">
       <div className="leaflet-control leaflet-bar street-stats" style={style}>
+        <h2>
+          {props.city.name}, {props.city.country}
+        </h2>
         <svg width={props.width} height={props.width}>
           <g transform={`translate(${props.width / 2}, ${props.width / 2})`}>
             <g className="base">
@@ -75,8 +92,14 @@ const StreetStatsControl = (props: {
             </g>
 
             <g className="streets">{histogramPath}</g>
+            <g className="sun_azimuths">{azimuthLines}</g>
           </g>
         </svg>
+        <p>
+          <Link href="/">
+            <a>&lt;&lt; Back to the city list</a>
+          </Link>
+        </p>
       </div>
     </div>
   );
@@ -91,6 +114,16 @@ const CityMap = (props: {
     (props.info.bb[0][1] + props.info.bb[1][1]) / 2,
     (props.info.bb[0][0] + props.info.bb[1][0]) / 2,
   ];
+  const redScale = d3.scaleLinear().domain([0, 3]).range(["red", "white"]);
+  const orangeScale = d3
+    .scaleLinear()
+    .domain([0, 3])
+    .range(["orange", "white"]);
+  const featureStyler = (feature) => {
+    // console.log(feature);
+    const colorScale = feature.properties.slope < 90 ? redScale : orangeScale;
+    return { color: colorScale(feature.properties.sun_diff) }; //, dashArray: "3 1", fillColor: "orange" };
+  };
   return (
     <MapContainer
       center={center}
@@ -103,7 +136,7 @@ const CityMap = (props: {
         //url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
       />
-      <GeoJSON data={props.streets} />
+      <GeoJSON data={props.streets} style={featureStyler} />
       <StreetStatsControl stats={props.stats} city={props.info} width={400} />
     </MapContainer>
   );
