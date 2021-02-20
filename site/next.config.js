3d54c51ab8409bfade3db04e9db9a47c15d65675
Dimi <sztanko@gmail.com>
2021-02-20 11:46:53 +0000

    Fix github pages serving

--- site/next.config.js
+++ site/next.config.js
@@ -1,11 +1,13 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
-    typescript: {
+  typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true,
-    },
-    trailingSlash: true, // you can safely remove it, github is smart enough
-    basePath: '/solsticestreets',
-}
+  },
+  trailingSlash: true, // you can safely remove it, github is smart enough
+  basePath: isProd ? "/solsticestreets" : "",
+};
