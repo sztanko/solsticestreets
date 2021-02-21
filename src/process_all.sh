df9bfb8a2e95bd68d117763a2e0ade18de6c4576
Dimi <sztanko@gmail.com>
2021-02-21 19:30:16 +0000

    Remove old code

--- src/process_all.sh
+++
@@ -1,9 +0,0 @@
-p=`dirname $0`
-
-wget -q https://raw.githubusercontent.com/migurski/Extractotron/master/cities.txt -O cities.txt
-for i in `cat $p/cities.txt | tail -n +2 | awk -F'\t' '{print $7}'`
-do
-    echo "processing $i"
-    time ./run-metro.sh $i $1
-done
-
