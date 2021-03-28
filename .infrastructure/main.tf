7de51f20c54e91d9ae1e30b1aa198efb3785e985
Dimi <demeter.sztanko@compasspathways.com>
2021-03-28 23:43:55 +0100

    Changes AWS env variable source; Persisted state in S3 instead of local

--- .infrastructure/main.tf
+++ .infrastructure/main.tf
@@ -3,6 +3,45 @@ provider "aws" {
 }
 
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "solsticestreets-tf-state"
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "solsticestreets-tf-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "solsticestreets-tf-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "solsticestreets-tf-locks"
+    encrypt        = true
+  }
+}
+
 resource "aws_vpc" "solsticestreets-vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
